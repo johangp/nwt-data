dc6e69c4dd6f473f4553e0734c1391369782aa35
Johan Palomino <johanpalomino59@gmail.com>
2024-12-23 17:18:08 +0100

    Replace ddl statement

--- ddl/media.sql
+++ ddl/media.sql
@@ -1,27 +1,16 @@
-DROP TYPE IF EXISTS category;
-CREATE TYPE category AS (
-    id VARCHAR,
-    name VARCHAR
-);
-
-DROP TYPE IF EXISTS feed;
-CREATE TYPE feed AS (
-    id VARCHAR,
-    name VARCHAR
-);
-
 DROP TABLE IF EXISTS raw_media;
 CREATE TABLE raw_media (
     id VARCHAR,
     sentiment VARCHAR,
-    categories category[],  
-    feed feed,               
+    categories JSONB,
+    feed_id VARCHAR,
+    feed_name VARCHAR,
     date TIMESTAMP,
-    msgId VARCHAR,
+    msg_id VARCHAR,
     type VARCHAR,
     title TEXT,
     summary TEXT,
     text TEXT,
-    source TEXT,
+    source_title TEXT,
     link TEXT
 );
