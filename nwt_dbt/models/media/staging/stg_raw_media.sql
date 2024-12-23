4f1ae380b80457a1f99b89c950e93e580b5a00b6
Johan Palomino <johanpalomino59@gmail.com>
2024-12-23 18:36:23 +0100

    Add staging model for raw_media

---
+++ nwt_dbt/models/media/staging/stg_raw_media.sql
@@ -0,0 +1,34 @@
+with raw as (
+    select
+        id,
+        sentiment,
+        jsonb_array_elements_text(categories) as category,
+        feed_id,
+        feed_name,
+        date,
+        msg_id,
+        type,
+        title,
+        summary,
+        text,
+        source_title,
+        link
+    from {{ source('raw_data', 'raw_media') }}
+)
+
+select
+    id,
+    sentiment,
+    category,
+    feed_id,
+    feed_name,
+    date,
+    msg_id,
+    type,
+    title,
+    summary,
+    text,
+    source_title,
+    link
+from raw
+
